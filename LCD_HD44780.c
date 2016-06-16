fbc306db98f6c52db94d4120e198d24ed08d3275
Kopranych <kopranych@gmail.com>
2016-06-16 21:25:43 +0300

    Откатил прическу This reverts commit eae6d3b04beb3be53f32f65a2233d7767c91c44d.

--- LCD_HD44780.c
+++ LCD_HD44780.c
@@ -9,18 +9,19 @@
 
 void LCD_init()
 {	
-	_delay_ms(40);//
-	DDR_DATA |= 0xF0;//
-	PORT_DATA |= 0xF0;//
-	DDR_SIG |= (ON<<E)|(ON<<RW)|(ON<<RS);//
-	PORT_SIG |= (ON<<E)|(ON<<RW)|(ON<<RS);//
-//	_delay_ms(40);
-	LCD_write(BUS_LINE,RS_COM);//
-	LCD_write(0x01,RS_COM);//
-	LCD_write(0x06,RS_COM);//
-	LCD_write(0x06,RS_COM);//
-	LCD_write(0x0C,RS_COM);//
-
+	_delay_ms(40);
+	DDR_DATA |= 0xF0;
+	PORT_DATA |= 0xF0;
+	DDR_SIG |= (ON<<E)|(ON<<RW)|(ON<<RS);
+	PORT_SIG |= (ON<<E)|(ON<<RW)|(ON<<RS);
+	_delay_ms(40);
+	LCD_write(0x28,RS_COM);
+	LCD_write(0x01,RS_COM);
+	LCD_write(0x06,RS_COM);
+	LCD_write(0x06,RS_COM);
+	LCD_write(0x0C,RS_COM);
+//	LCD_write(0x14,RS_COM);
+//	LCD_write(0x39,RS_DATA);
 }
 
 void LCD_write(uint8_t data, uint8_t BIT_RS)
@@ -40,15 +41,15 @@ void LCD_write(uint8_t data, uint8_t BIT_RS)
 	temp |= (data &0xF0);
 	PORT_DATA = temp;
 	PORT_SIG|= (ON<<E);
-	_delay_ms(2);
+	_delay_ms(10);
 	PORT_SIG &= ~(ON<<E);
 	temp = PORT_DATA & 0x0F;
 	temp|= (tmp<<4);
 	PORT_DATA = temp;
 	PORT_SIG|= (ON<<E);
-	_delay_ms(2);
+	_delay_ms(10);
 	PORT_SIG &= ~(ON<<E);
-	_delay_ms(40);	
+	_delay_ms(100);	
 
 }
 
@@ -78,40 +79,23 @@ void LCD_write_adress(uint8_t data, uint8_t adress)
 
 }
 
-void LCD_write_str(int8_t *string)
-{
-	while(*string)
-	{
-		LCD_write(*string,RS_DATA);
-		string++;
-	}
-}
-
-
 void LCD_read_BF()
 {
-	uint8_t tmp = 0;
-	uint8_t data = 0;
-
 	DDR_DATA &= 0x0F;
 	PORT_DATA |= 0xF0;
 	PORT_SIG |= (ON<<RW);
 	PORT_SIG &= ~(ON<<RS);
-	
-
-
+	uint8_t tmp = 0;
 	do{
 	PORT_SIG|= (ON<<E);
-	_delay_ms(10);
-	data = PIN_DATA & 0xF0;
+	_delay_ms(2);
 	PORT_SIG &= ~(ON<<E);
-	
+	uint8_t data = PIN_DATA & 0xF0;
 	tmp = data;
 	PORT_SIG|= (ON<<E);
-	_delay_ms(10);
-	data = PIN_DATA & 0xF0;
+	_delay_ms(2);
 	PORT_SIG &= ~(ON<<E);
-	
+	data = PIN_DATA & 0xF0;
 	data = data>>4;
 	tmp|=data;
 	}while((tmp &(1<<BF))!=0);		
@@ -122,3 +106,12 @@ void LCD_read_BF()
 
 
 
+/*void CLEAR_BIT(uint8_t PORTX, uint8_t PX)
+{
+	PORTX|= 0<<PX;
+}
+
+void SET_BIT(uint8_t PORTX, uint8_t PX)
+{
+	PORTX|= 1<<PX;
+}*/
--- LCD_HD44780.h
+++ LCD_HD44780.h
@@ -18,15 +18,12 @@
 #define BF        PA7//флаг занятости если 1 то занят дисплей если 0 то свободен
 #define RS_COM    0//передаем команду
 #define RS_DATA   1//передаем данные
-#define ON		  1//включение 
-#define OFF		  0//выключение
 #define BUS_LINE  0x28//2 линии данных, 4 бита шина
-#define SEC_LINE  0xA8//
-
+#define ON		  1//
+#define OFF		  0//
 
 //////////////////////////////////////функции///////////////////////////////
-void LCD_init();//функция инициализации дисплея 
-void LCD_write(uint8_t data, uint8_t BIT_RS);//функция записи в дисплей
-void LCD_read_BF();//функция чтения флага BF готовности из дисплея на данном железе НЕ РАБОТАЕТ!!!делаем через delay)))
-void LCD_write_adress(uint8_t data, uint8_t adress);//функция записи в дисплей по конкретному адресу
-void LCD_write_str(int8_t *string);//вывод строки на дисплей
+void LCD_init();//
+void LCD_write(uint8_t data, uint8_t BIT_RS);//
+void LCD_read_BF();//
+void LCD_write_adress(uint8_t data, uint8_t adress);//
